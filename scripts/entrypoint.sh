af848aa9c34526c88d6526f39bd3754281720412
Yossi Farjoun <farjoun@gmail.com>
2021-11-19 14:28:25 -0500

    message the user

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 echo "in entrypoint.sh"
 
-conda activate -n analysis
-
 if [ $# -eq 0 ]; then 
+	echo Please be patient as it takes some time to activate the conda environment
 	bash
 else 
 	echo "$@"
