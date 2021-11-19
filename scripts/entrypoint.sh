f78c09820722560d33f1c49499d99623493c4beb
Yossi Farjoun <farjoun@gmail.com>
2021-11-19 14:35:07 -0500

    more

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash --login
 echo "in entrypoint.sh"
 
-if [ $# -eq 0 ]; then 
-	echo Please be patient as it takes some time to activate the conda environment
-	bash
-else 
-	echo "$@"
-fi
+conda activate -n analysis
+
+# this is the entrypoint script. Currently it will print out the arguments.
+# this is probably NOT what you want the docker image to do...so this is where you're creative genious comes into play. 
+# figure out what you want the docker image to do when invoked and plop it all here. 
+
+echo "$@"
