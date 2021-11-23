93959db3c5565c2d6e6886b8fee4bb692e105462
Yossi Farjoun <farjoun@gmail.com>
2021-11-23 12:29:05 -0500

    trying to fix conda run

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash --login
 echo "in entrypoint.sh"
 
-conda activate -n analysis
-
-# this is the entrypoint script. Currently it will print out the arguments.
-# this is probably NOT what you want the docker image to do...so this is where you're creative genious comes into play. 
-# figure out what you want the docker image to do when invoked and plop it all here. 
-
 echo "$@"
+
+conda run -n analysis --no-capture-output "$@"
